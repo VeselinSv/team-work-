3e4a4bd11e9f5a86508f1152154a275f8107a804
VeselinSv <sveselin3@gmail.com>
2024-02-16 14:09:43 +0200

    make a general code

---
+++ homework/homework/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Въведете тегло (в кг):");
+
+int weight = int.Parse(Console.ReadLine());
+
+Console.Write("Въведете тегло (в кг):");
+
+double hight = double.Parse(Console.ReadLine());
+
+double BMI = weight / (hight * hight);
+
+if (BMI <= 18)
+{
+    Console.WriteLine("Podnormeno teglo");
+}
+else if (BMI > 18 &&  BMI <= 25)
+{
+    Console.WriteLine("Normalno teglo");
+}
+else
+{
+    Console.WriteLine("Nadnormeno teglo");
+}
